ddfb376cd53e82a55709f5e0cd711285369aaa59
camknopp <camknopp@gmail.com>
2020-04-05 09:42:19 -0400

    Typo

--- Natural Language Processing/homework5/model_embeddings.py
+++ Natural Language Processing/homework5/model_embeddings.py
@@ -31,7 +31,7 @@ class ModelEmbeddings(nn.Module):
         
         src_pad_token_idx = vocab.src['<pad>']
         tgt_pad_token_idx = vocab.tgt['<pad>']
-        self.source = nn.Embedding(len(vocab.src), embed_size, srs_pad_token_idx)
+        self.source = nn.Embedding(len(vocab.src), embed_size, src_pad_token_idx)
         self.target = nn.Embedding(embed_size, len(vocab.tgt), tgt_pad_token_idx)
 
         ### YOUR CODE HERE (~2 Lines)
