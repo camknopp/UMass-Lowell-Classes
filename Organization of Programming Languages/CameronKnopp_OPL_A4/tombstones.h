c66feed5b641beabd653f648b00b096b1c665d44
camknopp <camknopp@gmail.com>
2020-04-27 12:27:38 -0400

    Defined functions

--- Organization of Programming Languages/CameronKnopp_OPL_A4/tombstones.h
+++ Organization of Programming Languages/CameronKnopp_OPL_A4/tombstones.h
@@ -1,44 +1,187 @@
 #ifndef TOMBSTONES_H
 #define TOMBSTONES_H
 
+#include <iostream>
+
 // Defintion of Tomb for Pointer class
 template <class T>
-struct Tomb {
-    T* content;
+struct Tomb
+{
+    T *content;
     int ref_cnt;
+    bool used;
 
-    Tomb<T>() {
+    Tomb<T>()
+    {
         content = NULL;
         ref_cnt = 0;
+        used = true;
     }
-    ~Tomb() {
+    ~Tomb()
+    {
         content = NULL;
         ref_cnt = 0;
+        used = false;
     }
 };
+
+void mem_leak()
+{
+    std::cout << "Memory leak exception" << std::endl;
+    exit(1);
+}
+
+void dangling_ref()
+{
+    std::cout << "Dangling reference exception" << std::endl;
+    exit(1);
+}
 
 template <class T>
 class Pointer
 {
 public:
-    Pointer<T>();             // default constructor
-    Pointer<T>(Pointer<T> &); // copy constructor
-    Pointer<T>(T *);          // bootstrapping constructor
+    Tomb<T> *ptr;
+    Pointer<T>()
+    {
+        ptr = new Tomb<T>();
+        ptr->used = true;
+    }                         // default constructor
+    Pointer<T>(Pointer<T> &p) // copy constructor
+    {
+        if (p.ptr->used)
+        {
+            ptr = p.ptr;
+            if (ptr->ref_cnt != 0)
+            {
+                ptr->ref_cnt++;
+            }
+        }
+        else
+        {
+            dangling_ref();
+        }
+    }
+    Pointer<T>(T *t) // bootstrapping constructor
+    {
+        ptr = new Tomb<T>();
+        ptr->content = t;
+        ptr->used = true;
+        if (ptr->content == NULL)
+        {
+            ptr->ref_cnt = 0;
+        }
+        else
+        {
+            ptr->ref_cnt = 1;
+        }
+    }
     // argument should always be a call to new
-    ~Pointer<T>();                             // destructor
-    T &operator*() const;                      // dereferencing
-    T *operator->() const;                     // field dereferencing
-    Pointer<T> &operator=(const Pointer<T> &); // assignment
-    friend void free(Pointer<T> &);            // delete pointed-at object
+    ~Pointer<T>() // destructor
+    {
+        ptr->ref_cnt--;
+        if (ptr->used || ptr->ref_cnt == 0)
+        {
+            mem_leak();
+        }
+        ptr = NULL;
+    }
+    T &operator*() const // dereferencing
+    {
+        if (!ptr->used)
+        {
+            dangling_ref();
+        }
+        else if (ptr->ref_cnt == 0)
+        {
+            mem_leak();
+        }
+        else
+        {
+            return *(ptr->content);
+        }
+    }
+    T *operator->() const // field dereferencing
+    {
+        if (!ptr->used)
+        {
+            dangling_ref();
+        }
+        else if (ptr->ref_cnt == 0)
+        {
+            mem_leak();
+        }
+        else
+        {
+            return ptr->content;
+        }
+    }
+    Pointer<T> &operator=(const Pointer<T> &p) // assignment
+    {
+        ptr->ref_cnt--;
+        if (ptr->ref_cnt == 0)
+        {
+            mem_leak();
+        }
+        ptr = p.ptr;
+        ptr->ref_cnt++;
+        if (!ptr->used)
+        {
+            dangling_ref();
+        }
+        return *this;
+    }
+    friend void free(Pointer<T> &p) // delete pointed-at object
+    {
+        if (!p.ptr->used)
+        {
+            dangling_ref();
+        }
+        else
+        {
+            delete p.ptr->content;
+            p.ptr->used = false
+        }
+    }
     // This is essentially the inverse of the new inside the call to
     // the bootstrapping constructor. It should delete the pointed-to
     // object (which should in turn call its destructor).
     // equality comparisons:
-    bool operator==(const Pointer<T> &) const;
-    bool operator!=(const Pointer<T> &) const;
-    bool operator==(const int) const;
+    bool operator==(const Pointer<T> &p) const
+    {
+        if (!ptr->used || !p.ptr->used)
+        {
+            dangling_ref();
+        }
+        else
+        {
+            return ptr->content == p.ptr->content;
+        }
+    }
+    bool operator!=(const Pointer<T> &p) const
+    {
+        if (!ptr->used || !p.ptr->used)
+        {
+            dangling_ref();
+        }
+        else
+        {
+            return ptr->content != p.ptr->content;
+        }
+    }
+    bool operator==(const int x) const
+    {
+        if (ptr->content == NULL && x == 0)
+            return true;
+        return false;
+    }
     // true iff Pointer is null and int is zero
-    bool operator!=(const int) const;
+    bool operator!=(const int) const
+    {
+         if (ptr->content == NULL && x == 0)
+            return false;
+        return true;
+    }
     // false iff Pointer is null and int is zero
 };
 
